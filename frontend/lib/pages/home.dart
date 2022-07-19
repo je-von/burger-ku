3ce4570810f1968d1d9c18ebfa2307aee5bd2730
Jevon Levin <jevon.levin@gmail.com>
2022-07-19 23:47:31 +0700

    fix home page (add carousel)

--- frontend/lib/pages/home.dart
+++ frontend/lib/pages/home.dart
@@ -1,8 +1,19 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/components/logo.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  static const _images = [
+    'assets/carousel/1.png',
+    'assets/carousel/2.jpg',
+    'assets/carousel/3.jpg',
+    'assets/carousel/4.jpg',
+    'assets/carousel/5.jpg',
+    'assets/carousel/6.jpg',
+    'assets/carousel/7.jpg'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -10,10 +21,6 @@ class HomePage extends StatelessWidget {
       children: [
         Container(
           padding: const EdgeInsets.all(20),
-          margin: const EdgeInsets.fromLTRB(10, 10, 10, 40),
-          decoration: const BoxDecoration(
-            border: Border(bottom: BorderSide()),
-          ),
           child: Column(
             children: const [
               Text(
@@ -25,30 +32,11 @@ class HomePage extends StatelessWidget {
             ],
           ),
         ),
-        const Text(
-          'Events & Promotions',
-          textAlign: TextAlign.center,
-          style: TextStyle(fontWeight: FontWeight.w500, fontSize: 24),
-        ),
-        // CarouselSlider(
-        //   items: globals.carouselImages
-        //       .map((c) => Image(
-        //             image: AssetImage(c),
-        //           ))
-        //       .toList(),
-        //   options: CarouselOptions(
-        //       autoPlayCurve: Curves.fastLinearToSlowEaseIn,
-        //       autoPlay: true,
-        //       enableInfiniteScroll: true,
-        //       scrollDirection: Axis.horizontal,
-        //       enlargeCenterPage: true,
-        //       autoPlayInterval: Duration(seconds: 2)),
-        // ),
         Container(
           padding: const EdgeInsets.all(20),
           margin: const EdgeInsets.fromLTRB(10, 10, 10, 40),
           decoration: const BoxDecoration(
-            border: Border(top: BorderSide()),
+            border: Border.symmetric(horizontal: BorderSide()),
           ),
           child: Column(
             children: const [
@@ -62,6 +50,28 @@ class HomePage extends StatelessWidget {
                 textAlign: TextAlign.justify,
               )
             ],
+          ),
+        ),
+        const Text(
+          'Events & Promotions',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontWeight: FontWeight.w500, fontSize: 24),
+        ),
+        CarouselSlider(
+          items: _images
+              .map(
+                (i) => Image(
+                  image: AssetImage(i),
+                ),
+              )
+              .toList(),
+          options: CarouselOptions(
+            autoPlayCurve: Curves.fastLinearToSlowEaseIn,
+            autoPlay: true,
+            enableInfiniteScroll: true,
+            scrollDirection: Axis.horizontal,
+            enlargeCenterPage: true,
+            autoPlayInterval: const Duration(seconds: 2),
           ),
         ),
         Container(
