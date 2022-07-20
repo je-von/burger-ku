dca00edfdddbd0157165adfc7f5e129d58c50de2
Jevon Levin <jevon.levin@gmail.com>
2022-07-20 11:53:52 +0700

    finished delete cart

--- frontend/lib/pages/cart.dart
+++ frontend/lib/pages/cart.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:frontend/model/item.dart';
 import 'package:frontend/util/api.dart';
+import 'package:frontend/util/helper.dart';
 import 'package:intl/intl.dart';
 
 class CartPage extends StatefulWidget {
@@ -17,7 +18,7 @@ class CartPage extends StatefulWidget {
 class CartPageState extends State<CartPage> {
   List<Item> _cartItems = [];
 
-  _getAllItems() async {
+  _getAllCartItems() async {
     final response = await Api.request('get', 'carts');
     setState(() {
       if (response.statusCode == 200) {
@@ -32,7 +33,7 @@ class CartPageState extends State<CartPage> {
   @override
   void initState() {
     super.initState();
-    _getAllItems();
+    _getAllCartItems();
   }
 
   @override
@@ -73,7 +74,25 @@ class CartPageState extends State<CartPage> {
                         fontSize: 14,
                       ),
                     ),
-                    trailing: Icon(Icons.delete, color: Colors.red.shade300),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete, color: Colors.red.shade300),
+                      onPressed: () async {
+                        final response = await Api.request(
+                          'delete',
+                          'carts',
+                          body: {'item_id': '${e.id}'},
+                        );
+                        if (!mounted) return;
+                        if (response.statusCode == 200) {
+                          Helper.showSnackBar(
+                              context, '${e.name} deleted from cart!');
+                          _getAllCartItems();
+                        } else {
+                          Helper.showSnackBar(
+                              context, json.decode(response.body)['message']);
+                        }
+                      },
+                    ),
                   ),
                 )))
             .toList(),
